877dff530abbcc82ae1c029310be7e6bea3ddaac
Mohit <patelmohit719@gmail.com>
2022-05-27 15:53:58 +0530

    setup test

---
+++ src/Components/tests/Header.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+describe("Header Component", () => {
+  function setup() {
+    return render(
+      <Header title="My Title">
+        <div>Children</div>
+      </Header>
+    );
+  }
+
+  it("render heading", () => {
+    setup();
+    expect(screen.getByText("React Playground")).toBeInTheDocument();
+  });
+});
